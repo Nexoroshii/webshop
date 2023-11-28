fe7d4e1f7a4a8efac53442f4797da8653a97ba81
Egor Work <e.moisei1@ggg.com>
2023-11-28 20:57:23 +0300

    finish promo section

--- src/components/promo/Promo.jsx
+++ src/components/promo/Promo.jsx
@@ -7,7 +7,26 @@ const Promo = () => {
     <section className="promo">
       <div className="container">
         <div className="promo__content">
-          <div className="promo_text">Text</div>
+          <div className="promo__text">
+            <div className="promo__title">
+              <span class="highlight">
+                <span>LET’S</span>
+              </span>
+              EXPLORE
+              <span class="highlight highlight--yellow">
+                <span>UNIQUE</span>
+              </span>
+              CLOTHES.
+            </div>
+            <div className="promo__descr">
+              Live for Influential and Innovative fashion!
+            </div>
+            <div className="promo__btn-wrapper">
+              <a href="#!" className="promo__btn">
+                Shop Now
+              </a>
+            </div>
+          </div>
           <div className="promo__img">
             <img src={promoImg} alt="promoImg"></img>
           </div>
